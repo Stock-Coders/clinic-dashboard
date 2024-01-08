68e0955e9f578197d26645ac8e78f91ab262aa5b
KareemDEV <kareemtarekpk@gmail.com>
2024-01-08 14:18:00 +0200

    some updates in t_dashboard!

--- resources/views/layouts/dashboard/includes/copy-right.blade.php
+++ resources/views/layouts/dashboard/includes/copy-right.blade.php
@@ -3,7 +3,7 @@
     $currentYear = date('Y');
 
     // Set your specific year (e.g., the year of establishment)
-    $establishmentYear = 2022; // Replace with your desired establishment year
+    $establishmentYear = 2023; // Replace with your desired establishment year
 
     // Ensure the copyright year does not exceed the current year
     $copyrightYear = ($establishmentYear > $currentYear) ? $currentYear : $establishmentYear;
