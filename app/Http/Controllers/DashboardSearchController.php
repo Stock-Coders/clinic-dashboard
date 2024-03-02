77e7e97c7b3e3046dfbd98704c188e483e0f943d
KareemDEV <kareemtarekpk@gmail.com>
2024-03-02 18:54:06 +0200

    some new updates

--- app/Http/Controllers/DashboardSearchController.php
+++ app/Http/Controllers/DashboardSearchController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\Patient;
 use App\Models\XRay;
